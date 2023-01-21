b7736d3e7de749a5e304762edabe8b69d7f7f25a
ezenardr <ezenardrodolphe@gmail.com>
2023-01-21 10:07:01 -0500

    mobile navigation

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,10 @@
 
 const navLinks = document.querySelector(".nav-links");
 const contactBtn = document.querySelector(".btn-contact");
+const navBar = document.querySelector(".navbar");
+const btnOpen = document.querySelector(".icon-mobile-nav-open");
+const btnClose = document.querySelector(".icon-mobile-nav-close");
+const wrapper = document.querySelector(".wrapper");
 // Implementing smooth scrolling
 const scrollFunc = function (e) {
     e.preventDefault();
@@ -9,6 +13,20 @@ const scrollFunc = function (e) {
     // console.log(clicked);
     if (!clicked) return;
     document.querySelector(clicked).scrollIntoView({ behavior: "smooth" });
+    navBar.classList.remove("nav-open");
+    wrapper.classList.add("hidden");
 };
 navLinks.addEventListener("click", scrollFunc);
 contactBtn.addEventListener("click", scrollFunc);
+
+// Mobile Navigation
+
+btnOpen.addEventListener("click", () => {
+    // console.log('ok');
+    navBar.classList.add("nav-open");
+    wrapper.classList.remove("hidden");
+});
+btnClose.addEventListener("click", () => {
+    navBar.classList.remove("nav-open");
+    wrapper.classList.add("hidden");
+});
